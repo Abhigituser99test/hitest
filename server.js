ad49d8b0819c73a51e9a0d68e370100be349a6dc
Abhigituser99test <139984958+Abhigituser99test@users.noreply.github.com>
2023-08-23 11:54:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials","true");
   res.send("Welcome to eShop website.");
 });
 
